1e791d3b927ea44da5e0a8169d2bbf2d5a3687fc
Monu Saini <hackingkali789@gmail.com>
2023-12-10 00:06:22 +0530

    change time zone

--- youtubemp3/settings.py
+++ youtubemp3/settings.py
@@ -111,14 +111,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Konkata'
-
-USE_L10N = True
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
